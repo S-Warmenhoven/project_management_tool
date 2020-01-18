980e78f6e9e79a06b9dc4aba6359ca0ce42dfa44
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-01-18 14:45:11 -0800

    initial Product_Management_Tool commit with Home and About Pages

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+    def index
+        #by default it will go to the views html page
+        #and look for a directory and a file with the same name
+        #as the action "index" like render :index
+    end
+
+    def about
+    end
+end
